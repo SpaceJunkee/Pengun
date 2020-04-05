d88be37e620aaa716413ada8352c40ee49c859e7
Kevin <ksandy@outlook.ie>
2020-04-05 10:33:13 +0100

    Start shooting mechanics

---
+++ First Game Collab Project/Assets/C# Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform shootingPosition;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Button to fire a bullet (default square on ps controller)
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, shootingPosition.position, shootingPosition.rotation);
+    }
+}
