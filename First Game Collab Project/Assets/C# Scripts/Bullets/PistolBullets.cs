897c37fff1cd4d851eac93b89dcf7e9c77736d56
mrhborland <markborland5@gmail.com>
2020-07-17 15:36:14 +0100

    Add inheritance to bullet scripts for ease of editing.

--- First Game Collab Project/Assets/C# Scripts/Bullets/PistolBullets.cs
+++ First Game Collab Project/Assets/C# Scripts/Bullets/PistolBullets.cs
@@ -2,59 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PistolBullets : MonoBehaviour
+public class PistolBullets : Bullet
 {
- 
-
-    public float speed = 20f;
-    public float lifeTimeOfBullet;
-    public Rigidbody2D rigidBody;
-    public int bulletDamage = 100;
-
     // Start is called before the first frame update
     void Start()
     {
+        speed = 50f;
+        lifeTime = 0.6f;
+        bulletDamage = 100;
         moveBullet();
 
-    }
-    void Update()
-    {
-        lifeTimeOfBullet -= Time.deltaTime;
-        if (lifeTimeOfBullet <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        EnemyScript1 enemy = collision.GetComponent<EnemyScript1>();
-        BreackableCrate crate = collision.GetComponent<BreackableCrate>();
-
-        if (enemy != null)
-        {
-            enemy.TakeDamage(bulletDamage);
-        }
-
-        if (crate != null)
-        {
-            crate.TakeDamage(bulletDamage);
-        }
-
-        destroyBullet();
-    }
-
-    void moveBullet()
-    {
-        //Use right instead of forward as we dont want to use z axis in 2d game
-        rigidBody.transform.Rotate(0, 0, Random.Range(-.5f, .5f));
-        rigidBody.velocity = transform.right * speed;
-
-    }
-
-    void destroyBullet()
-    {
-        Destroy(gameObject);
     }
 }
 
