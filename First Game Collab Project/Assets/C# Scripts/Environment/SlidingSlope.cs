08d0a55d1c475ebf70d62fcc2669d9a2fcba971d
Kevin <ksandy@outlook.ie>
2020-07-18 12:35:26 +0100

    added slope slides

---
+++ First Game Collab Project/Assets/C# Scripts/Environment/SlidingSlope.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingSlope : MonoBehaviour
+
+{
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            PlayerMovement.canMove = false;
+            
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            PlayerMovement.canMove = true;
+           
+            
+        }
+    }
+    
+}
