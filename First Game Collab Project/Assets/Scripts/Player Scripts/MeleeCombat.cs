adb01d7ae9e31161071694112e9757e641d1f978
SpaceJunkee <ksandy@outlook.ie>
2021-05-28 18:19:59 +0100

    Added level 1

--- First Game Collab Project/Assets/Scripts/Player Scripts/MeleeCombat.cs
+++ First Game Collab Project/Assets/Scripts/Player Scripts/MeleeCombat.cs
@@ -7,6 +7,7 @@ public class MeleeCombat : MonoBehaviour
     public Transform attackPos;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+    public LayerMask crateLayers;
     public int meleeDamage = 150;
     public float attackRate = 2f;
     public float meleeCooldown = 0f;
@@ -20,23 +21,39 @@ public class MeleeCombat : MonoBehaviour
             if (Input.GetButtonDown("Melee"))
             {
                 //play anim
-                Attack();
+                AttackCrates();
+                AttackEnemies();
                 meleeCooldown = Time.time + 0.6f / attackRate;
             }
         }
         
     }
 
-    private void Attack()
+    private void AttackCrates()
+    {
+        //Play Animation
+        //anim.SetTrigger("Melee");
+        //Detect enemies in range
+        Collider2D[] hitCrates = Physics2D.OverlapCircleAll(attackPos.position, attackRange, crateLayers);
+        //Apply damage
+       
+        foreach (Collider2D crate in hitCrates)
+        {
+            crate.GetComponent<BreackableCrate>().TakeDamage(meleeDamage);
+        }
+    }
+
+    private void AttackEnemies()
     {
         //Play Animation
         //anim.SetTrigger("Melee");
         //Detect enemies in range
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayers);
         //Apply damage
-        foreach(Collider2D enemy in hitEnemies)
+
+        foreach (Collider2D enemy in hitEnemies)
         {
-            enemy.GetComponent<BreackableCrate>().TakeDamage(meleeDamage);
+            enemy.GetComponent<EnemySmash>().TakeDamage(meleeDamage);
         }
     }
 
