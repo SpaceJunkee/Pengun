9d605547a8d3b218ad5dca59e35696ca918a4be5
SpaceJunkee <ksandy@outlook.ie>
2021-10-07 16:55:58 +0100

    Added room cameras to dynamically change views when entering or exiting areas.

---
+++ Pengun/Assets/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    CinemachineVirtualCamera vcam;
+
+    float originalOrthographicSize = 10f;
+
+    private void Start()
+    {
+        vcam = this.GetComponentInChildren<CinemachineVirtualCamera>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        vcam.Priority = 3;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        vcam.Priority = 1;
+        
+    }
+}
