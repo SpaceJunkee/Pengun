7381750e77e1deda1edf9bb2d05eb2b057ca93af
SpaceJunkee <ksandy@outlook.ie>
2022-02-04 12:19:09 +0000

    Add parallax

---
+++ Pengun/Assets/Scripts/Parallax/ParallaxBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    public Vector2 parallaxEffectMultiplier;
+
+    private void Start()
+    {
+        cameraTransform = GameObject.Find("CM vcam1").GetComponent<Transform>();
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
