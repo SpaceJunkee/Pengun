7028daeb5d845eb9cfe283ebda005a051caa32b0
SpaceJunkee <ksandy@outlook.ie>
2022-02-07 17:03:57 +0000

    Add footstep sound effect placeholders and hooked them in with run animation.

---
+++ Pengun/Assets/PlayerSoundEffects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundEffects : MonoBehaviour
+{
+
+    public AudioClip[] footStepClips;
+    public AudioSource foorStepAudioSource;
+    
+   void PlayFootStep1()
+    {
+        foorStepAudioSource.clip = footStepClips[Random.Range(0, footStepClips.Length)];
+        foorStepAudioSource.Play();
+    }
+
+    void PlayFootStep2()
+    {
+        foorStepAudioSource.clip = footStepClips[Random.Range(0, footStepClips.Length)];
+        foorStepAudioSource.Play();
+    }
+}
