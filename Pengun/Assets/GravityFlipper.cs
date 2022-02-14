68a7b5e9833102f8ede5f232bfe01eeff488886b
SpaceJunkee <ksandy@outlook.ie>
2022-02-14 17:30:26 +0000

    Add gravity flippers

---
+++ Pengun/Assets/GravityFlipper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFlipper : MonoBehaviour
+{
+    public float gravitySpeed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -gravitySpeed;
+            PlayerMovement.canUseInput = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 4;
+            PlayerMovement.canUseInput = true;
+
+        }
+    }
+}
