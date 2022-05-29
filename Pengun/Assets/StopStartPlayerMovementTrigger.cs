7d4ed2ea7dbd7a506c54a1ed4286a84f7ecbabc1
SpaceJunkee <ksandy@outlook.ie>
2022-05-29 12:33:58 +0100

    Added level triggers to disable and enable movement

---
+++ Pengun/Assets/StopStartPlayerMovementTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopStartPlayerMovementTrigger : MonoBehaviour
+{
+    public bool stopMovement, startMovement;
+    public float stopDelay, startDelay;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && stopMovement && !startMovement)
+        {
+            StartCoroutine("StopMovement");
+        }
+        else if (collision.CompareTag("Player") && startMovement && !stopMovement)
+        {
+            StartCoroutine("StartMovement");
+        }
+        
+    }
+
+    IEnumerator StopMovement()
+    {
+        yield return new WaitForSeconds(stopDelay);
+        PlayerMovement.canMove = false;
+    }
+
+    IEnumerator StartMovement()
+    {
+        yield return new WaitForSeconds(startDelay);
+        PlayerMovement.canMove = true;
+    }
+}
